a262f05344d76f819532fc5b214bfc2cc88245fe
VictorSvetailo <victorioeve@gmail.com>
2022-08-03 19:17:43 +0300

    #2

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type ButtonPropsType = {
+    nickName: string
+    callBack: () => void
+}
+
+export const Button = (props: ButtonPropsType) => {
+    const onClickButtonHandler = () => {
+        props.callBack();
+    };
+
+    return (
+        <button onClick={onClickButtonHandler}>{props.nickName}</button>
+    );
+};
